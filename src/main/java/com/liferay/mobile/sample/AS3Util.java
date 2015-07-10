80a765e128955f86658997d9ffab237bd8c23229
David <davidbwelch@hotmail.com>
2015-07-09 22:14:26 -0400

    Simple start

---
+++ src/main/java/com/liferay/mobile/sample/AS3Util.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.sample;
+
+import com.liferay.mobile.sdk.util.LanguageUtil;
+
+import org.apache.commons.lang.WordUtils;
+
+/**
+ * @author Bruno Farache
+ */
+public class AS3Util extends LanguageUtil {
+
+	public static final String INTEGER = "Integer";
+
+	public static final String JSON_ARRAY = "JSONArray";
+
+	public static final String JSON_OBJECT = "JSONObject";
+
+	public static final String JSON_OBJECT_WRAPPER = "JSONObjectWrapper";
+
+	public static final String UPLOAD_DATA = "UploadData";
+
+	public String getReturnType(String type) {
+		type = getType(type);
+
+		if (type.equals(VOID)) {
+			return type;
+		}
+
+		if (type.equals(INT)) {
+			return INTEGER;
+		}
+
+		if (type.equals(JSON_OBJECT_WRAPPER) || type.equals(UPLOAD_DATA)) {
+			return JSON_OBJECT;
+		}
+
+		if (type.equals(BYTE_ARRAY)) {
+			return JSON_ARRAY;
+		}
+
+		return WordUtils.capitalize(type);
+	}
+
+	public String getType(String type) {
+		type = super.getType(type);
+
+		if (type.equals(BOOLEAN) || type.equals(BYTE_ARRAY) ||
+			type.equals(DOUBLE) || type.equals(INT) || type.equals(LONG) ||
+			type.equals(VOID)) {
+
+			return type;
+		}
+
+		if (isArray(type)) {
+			return JSON_ARRAY;
+		}
+
+		if (type.equals(STRING)) {
+			return "String";
+		}
+
+		if (type.equals(FILE)) {
+			return UPLOAD_DATA;
+		}
+
+		if (type.startsWith(OBJECT_PREFIX)) {
+			return JSON_OBJECT_WRAPPER;
+		}
+
+		return JSON_OBJECT;
+	}
+
+	public boolean isPrimitive(String type) {
+		if (type.equals(BOOLEAN) || type.equals(DOUBLE) || type.equals(INT) ||
+			type.equals(LONG)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+}
---
+++ src/main/java/com/liferay/mobile/sample/BaseBuilder.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.sample;
+
+import com.liferay.mobile.sdk.http.Action;
+import com.liferay.mobile.sdk.http.Discovery;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+/**
+ * @author Bruno Farache
+ */
+public abstract class BaseBuilder implements Builder {
+
+	@Override
+	public void buildAll(
+			Discovery discovery, String packageName, int version,
+			String destination)
+		throws Exception {
+
+		HashMap<String, List<Action>> actionsMap =
+			new HashMap<String, List<Action>>();
+
+		List<Action> actions = discovery.getActions();
+
+		for (Action action : actions) {
+			String path = action.getPath();
+
+			String className = path.substring(1, path.indexOf("/", 1));
+			List<Action> classActions = actionsMap.get(className);
+
+			if (classActions == null) {
+				classActions = new ArrayList<Action>();
+
+				actionsMap.put(className, classActions);
+			}
+
+			classActions.add(action);
+		}
+
+		for (Entry<String, List<Action>> entry : actionsMap.entrySet()) {
+			build(
+				discovery, entry.getValue(), packageName, version,
+				entry.getKey(), destination);
+		}
+	}
+
+	protected static final String ACTIONS = "actions";
+
+	protected static final String BYTE_ARRAY = "BYTE_ARRAY";
+
+	protected static final String CLASS_NAME = "className";
+
+	protected static final String DISCOVERY = "discovery";
+
+	protected static final String ESCAPE_TOOL = "esc";
+
+	protected static final String JSON_OBJECT_WRAPPER = "JSON_OBJECT_WRAPPER";
+
+	protected static final String LANGUAGE_UTIL = "languageUtil";
+
+	protected static final String VOID = "VOID";
+
+}
